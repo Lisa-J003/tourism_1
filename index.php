6b14e4263e9c1f76e1a8f6f88026810a0a61b180
Lisa-J003 <elizavetatosenko@gmail.com>
2024-07-31 10:44:16 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+	<!DOCTYPE htm1>
+<htm1 lang="en">
+<head>
+      <meta charset="UTF-8">
+	  <title>Price</title
+	  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.css" rel="stylesheet" crossorigin="anonymous">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" crossorigin="anonymous"></script>
+</head>
+<body>	  
+    	<header>
+		    <nav class=" navbar navbar-default" role ="navigation">
+			    <div class="container-fluid"> 
+					<div class="navbar-header">
+					    <button type="button" class="navbar-toggle" data-toggle="collapse"
+						data-target=".navbar-ex1-collapse">
+						   <span class="sr-only">Toggle navigation</span>
+						   <span class="icon-bar"></span>
+						   <span class="icon-bar"></span>
+						   <span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Price</a
+					</div>
+					
+					<div class="collapse navbar-collapse navbar-ex1-collapse">
+					    <ul class="nav navbar-nav">
+						    <li><a href="price.php">Price</a></li>
+							<li><a href="air.php">Air</li>
+							<li><a href="hotels.php">Hotels</li>
+							<li><a href="ex.rates.php">Exchange_Rates</li> 
+						</ul>	
+					</div>		
+				</div>			
+		    </nav>
+	
+		</header>
+		
+		<div id="content">
+		
+		</div>
+		
+		<footer>
+		
+		</footer>
+</body>
+</htm1>
